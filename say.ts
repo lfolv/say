b50b2f0db9696edba3fc1ce632e15c6e93bf2eb2
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2020-09-25 20:54:21 -0300

    Update inEnglish() method from Say class
    - This method should respond to all base numbers
    
    Use concatenation to get ready to handle with numbers greater than nine

--- say.ts
+++ say.ts
@@ -1,9 +1,33 @@
 export default class Say {
   inEnglish(n: number) {
-    if (n === 1) {
-      return 'one'
+    if (n === 0) {
+      return 'zero'
     }
 
-    return 'zero'
+    let result = ''
+
+    if (n === 9) {
+      result += 'nine'
+    } else if (n === 8) {
+      result += 'eight'
+    } else if (n === 7) {
+      result += 'seven'
+    } else if (n === 6) {
+      result += 'six'
+    } else if (n === 5) {
+      result += 'five'
+    } else if (n === 4) {
+      result += 'four'
+    } else if (n === 3) {
+      result += 'three'
+    } else if (n === 2) {
+      result += 'two'
+    } else if (n === 1) {
+      result += 'one'
+    } else {
+      result += 'zero'
+    }
+
+    return result
   }
 }
